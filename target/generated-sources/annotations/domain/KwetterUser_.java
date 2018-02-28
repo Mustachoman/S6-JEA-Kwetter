2e8d822d0fd9ecc3b4a619c1721b8286f4ef1541
roy_siebers <roy_siebers@live.nl>
2018-02-28 10:05:09 +0100

    test

--- target/generated-sources/annotations/domain/KwetterUser_.java
+++ target/generated-sources/annotations/domain/KwetterUser_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-26T12:54:01")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-28T10:02:10")
 @StaticMetamodel(KwetterUser.class)
 public class KwetterUser_ { 
 
